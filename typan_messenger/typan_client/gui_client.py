122a533d9d382958d8bf97dfcc979e8d7479a8a7
ouril <ourilkregg@gmail.com>
2018-01-18 23:30:28 +0300

    first commit with ui

---
+++ typan_messenger/typan_client/gui_client.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets
+from .typan_msg import *
+
+
+class GuiClient(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
